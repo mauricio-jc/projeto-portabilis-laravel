7f721a913f626d39041bbd6bc3fa1f315141e02e
Mauricio Citadini Bilessimo <pariulon@hotmail.com>
2016-07-27 20:00:28 -0300

    Modificado página home.

--- resources/views/principal/home.blade.php
+++ resources/views/principal/home.blade.php
@@ -4,4 +4,31 @@
 
 	<h1>Bem-vindo ao sistema!</h1>
 	<hr>
+	
+	<div class="col-md-4">
+		<div class="row">
+			<label>Listar usuários</label>
+		</div>
+		<a href="/user_lst">
+			<img src="images/users.png" alt="users" class="img-rounded">
+		</a>
+	</div>
+
+	<div class="col-md-4">
+		<div class="row">
+			<label>Listar cursos</label>
+		</div>
+		<a href="/curso_lst">
+			<img src="images/courses.png" alt="users" class="img-rounded">
+		</a>
+	</div>
+
+	<div class="col-md-4">
+		<div class="row">
+			<label>Sair do sistema</label>
+		</div>
+		<a href="/logout" onclick="return confirm('Deseja mesmo sair do sistema?');">
+			<img src="images/exit.png" alt="users" class="img-rounded">
+		</a>
+	</div>
 @stop
