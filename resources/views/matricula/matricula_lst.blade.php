0c7c4ee8f2236e825feb5e86fcd9be0367ade300
Mauricio Citadini Bilessimo <pariulon@hotmail.com>
2016-08-10 13:26:09 -0300

    Alterado layout na listagem de matrículas.

--- resources/views/matricula/matricula_lst.blade.php
+++ resources/views/matricula/matricula_lst.blade.php
@@ -48,83 +48,169 @@
 	</form>
 
 	<div class="col-md-12">
-		<table class="table table-bordered table-hover table-background">
-			<thead>
-				<tr class="active">
-					<th>Aluno</th>
-					<th>Curso</th>
-					<th>Ano</th>
-					<th>Situação</th>
-					<th>Pago</th>
-					<th>Ações</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($matriculas as $matricula)
-					<tr>
-						<td>{{$matricula->nome_aluno}}</td>
-						<td>{{$matricula->curso_nome}}</td>
-						<td>{{$matricula->ano}}</td>
-						<td>{{$matricula->ativo == 1 ? 'Ativo' : 'Inativo'}}</td>
-						<td>{{$matricula->pago == 1 ? 'Sim' : 'Não'}}</td>
-						<td>
-							@if($matricula->ativo == 1)
-								<a href="/matricula_desat/{{$matricula->id}}" class="btn btn-warning" onclick="return confirm('Deseja mesmo inativar esta matrícula?');">
-									<span class="glyphicon glyphicon-remove"></span>
-									<strong>Inativar</strong>
+		<div class="panel panel-primary">
+			<div class="panel-body">
+				<table class="table table-hover">
+					<thead>
+						<tr class="active">
+							<th>Aluno</th>
+							<th>Curso</th>
+							<th>Ano</th>
+							<th>Situação</th>
+							<th>Pago</th>
+							<th>Ações</th>
+						</tr>
+					</thead>
+					<tbody>
+					@foreach($matriculas as $matricula)
+						<tr>
+							<td>{{$matricula->nome_aluno}}</td>
+							<td>{{$matricula->curso_nome}}</td>
+							<td>{{$matricula->ano}}</td>
+							<td>{{$matricula->ativo == 1 ? 'Ativo' : 'Inativo'}}</td>
+							<td>{{$matricula->pago == 1 ? 'Sim' : 'Não'}}</td>
+							<td>
+								@if($matricula->ativo == 1)
+									<a href="" class="btn btn-warning" data-toggle="modal" data-target="#{{$matricula->id}}-1">
+										<span class="glyphicon glyphicon-remove"></span>
+										<strong>Inativar</strong>
+									</a>
+								@else
+									<a href="" class="btn btn-success" data-toggle="modal" data-target="#{{$matricula->id}}-1">
+										<span class="glyphicon glyphicon-remove"></span>
+										<strong>Ativar</strong>
+									</a>
+								@endif
+								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{{$matricula->id}}">
+									<span class="glyphicon glyphicon-search"></span>
+  									<strong>Visualizar</strong>
+								</button>
+								<a href="" class="btn btn-danger" data-toggle="modal" data-target="#{{$matricula->id}}-2">
+									<span class="glyphicon glyphicon-trash"></span>
+									<strong>Excluir</strong>
 								</a>
-							@else
-								<a href="/matricula_ati/{{$matricula->id}}" class="btn btn-success" onclick="return confirm('Deseja mesmo ativar a matrícula?');">
-									<span class="glyphicon glyphicon-ok" ></span>
-									<strong>Ativar</strong>
-								</a>
-							@endif
-							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{{$matricula->id}}">
-								<span class="glyphicon glyphicon-search"></span>
-  								<strong>Visualizar</strong>
-							</button>
-							<a href="/matricula_del/{{$matricula->id}}" class="btn btn-danger" onclick="return confirm('Deseja mesmo excluir esta matrícula?');">
-								<span class="glyphicon glyphicon-trash"></span>
-  								<strong>Excluir</strong>
-							</a>
-						</td>
-					</tr>
-					<div class="modal fade" id="{{$matricula->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  						<div class="modal-dialog" role="document">
-    						<div class="modal-content">
-      							<div class="modal-header">
-        							<h3 class="modal-title" id="myModalLabel">
-        								<strong>Detalhes da matrícula</strong>
-        							</h3>
-      							</div>
-      							<div class="modal-body">
-        							<strong>Código da matrícula: </strong>{{$matricula->id}} <br>
-        							<strong>Nome: </strong>{{$matricula->nome_aluno}} <br>
-        							<strong>Curso: </strong>{{$matricula->curso_nome}} <br>
-        							<strong>Ano: </strong>{{$matricula->ano}} <br>
-        							<strong>Situação: </strong>{{$matricula->ativo == 1 ? 'Ativo' : 'Inativo'}} <br>
-        							<strong>Pago: </strong>{{$matricula->pago == 1 ? 'Sim' : 'Não'}} <br>
-        							<strong>Período: </strong>{{$matricula->periodo}} <br>
-        							<strong>Telefone: </strong>{{$matricula->telefone}} <br>
-        							<strong>Data de nascimento: </strong><?php echo date_format(new DateTime($matricula->data_nascimento), 'd/m/Y'); ?>
-      							</div>
-      							<div class="modal-footer">
-        							<button type="button" class="btn btn-inverse" data-dismiss="modal">
-        								<span class="glyphicon glyphicon-remove"></span>
-        								<strong>Fechar</strong>
-        							</button>
-      							</div>
-    						</div>
-  						</div>
-					</div>
-				@endforeach
-			</tbody>
-		</table>
+							</td>
+						</tr>
+						<div class="modal fade" id="{{$matricula->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  							<div class="modal-dialog" role="document">
+    							<div class="modal-content">
+      								<div class="modal-header">
+        								<h3 class="modal-title" id="myModalLabel">
+        									<strong>Detalhes da matrícula</strong>
+        								</h3>
+      								</div>
+	      							<div class="modal-body">
+    	    							<strong>Código da matrícula: </strong>{{$matricula->id}} <br>
+        								<strong>Nome: </strong>{{$matricula->nome_aluno}} <br>
+        								<strong>Curso: </strong>{{$matricula->curso_nome}} <br>
+        								<strong>Ano: </strong>{{$matricula->ano}} <br>
+        								<strong>Situação: </strong>{{$matricula->ativo == 1 ? 'Ativo' : 'Inativo'}} <br>
+        								<strong>Pago: </strong>{{$matricula->pago == 1 ? 'Sim' : 'Não'}} <br>
+        								<strong>Período: </strong>{{$matricula->periodo}} <br>
+        								<strong>Telefone: </strong>{{$matricula->telefone}} <br>
+        								<strong>Data de nascimento: </strong><?php echo date_format(new DateTime($matricula->data_nascimento), 'd/m/Y'); ?>
+	      							</div>
+    	  							<div class="modal-footer">
+        								<button type="button" class="btn btn-inverse" data-dismiss="modal">
+        									<span class="glyphicon glyphicon-remove"></span>
+        									<strong>Fechar</strong>
+        								</button>
+      								</div>
+    							</div>
+	  						</div>
+						</div>
+
+						@if($matricula->ativo == 1)
+							<div class="modal fade" id="{{$matricula->id}}-1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+								<div class="modal-dialog" role="document">
+							    	<div class="modal-content">
+							      		<div class="modal-header">
+											<h3 class="modal-title" id="myModalLabel">
+    	    									<strong>Inativar a matrícula {{$matricula->id}}</strong>
+	        								</h3>
+	      								</div>
+		      							<div class="modal-body">
+    		    							<strong>Deseja mesmo inativar esta matrícula?</strong>
+	      								</div>
+		      							<div class="modal-footer">
+      										<a href="/matricula_desat/{{$matricula->id}}" class="btn btn-success">
+      											<span class="glyphicon glyphicon-ok"></span>
+        										<strong>Sim</strong>
+        									</a>
+        									<button type="button" class="btn btn-danger" data-dismiss="modal">
+        										<span class="glyphicon glyphicon-remove"></span>
+												<strong>Não</strong>
+		    	    						</button>
+				      					</div>
+    								</div>
+  								</div>
+							</div>
+						@else
+							<div class="modal fade" id="{{$matricula->id}}-1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+								<div class="modal-dialog" role="document">
+							    	<div class="modal-content">
+							      		<div class="modal-header">
+											<h3 class="modal-title" id="myModalLabel">
+    	    									<strong>Ativar a matrícula {{$matricula->id}}</strong>
+	        								</h3>
+	      								</div>
+		      							<div class="modal-body">
+    		    							<strong>Deseja mesmo ativar esta matrícula?</strong>
+	      								</div>
+		      							<div class="modal-footer">
+      										<a href="/matricula_ati/{{$matricula->id}}" class="btn btn-success">
+      											<span class="glyphicon glyphicon-ok"></span>
+        										<strong>Sim</strong>
+        									</a>
+        									<button type="button" class="btn btn-danger" data-dismiss="modal">
+        										<span class="glyphicon glyphicon-remove"></span>
+												<strong>Não</strong>
+		    	    						</button>
+				      					</div>
+    								</div>
+  								</div>
+							</div>
+						@endif
+
+
+						<div class="modal fade" id="{{$matricula->id}}-2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+							<div class="modal-dialog" role="document">
+						    	<div class="modal-content">
+						      		<div class="modal-header">
+										<h3 class="modal-title" id="myModalLabel">
+    	    								<strong>Excluir a matrícula {{$matricula->id}}</strong>
+	        							</h3>
+	      							</div>
+		      						<div class="modal-body">
+    		    						<strong>Deseja mesmo excluir esta matrícula?</strong>
+      								</div>
+	      							<div class="modal-footer">
+      									<a href="/matricula_del/{{$matricula->id}}" class="btn btn-success">
+      										<span class="glyphicon glyphicon-ok"></span>
+        									<strong>Sim</strong>
+        								</a>
+        								<button type="button" class="btn btn-danger" data-dismiss="modal">
+        									<span class="glyphicon glyphicon-remove"></span>
+											<strong>Não</strong>
+		    	    					</button>
+			      					</div>
+    							</div>
+  							</div>
+						</div>
+					@endforeach
+					</tbody>
+				</table>
+				<a href="/matricula_cad" class="btn btn-default">
+					<span class="glyphicon glyphicon-plus"></span>
+					<strong>Nova matrícula</strong>
+				</a>
+			</div>
+		</div>
 	</div>
 
-	<div class="col-md-6 col-md-offset-5"> 
+	<center> 
 		<?php echo $matriculas->render(); ?>
-	</div>
+	</center>
 @stop
 
 @section('links')
